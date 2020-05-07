f301c3db4e29f2cbe2eef74875e59944ffc17145
wzw <wuzhuwei@kemai.cn>
2020-05-07 16:39:59 +0800

    环境变量设置

---
+++ telnet.sql
@@ -0,0 +1,26 @@
+
+会员 20883
+invoke com.xdd.fans.remote.service.marketing.shop.IShopGroupBuyGoodsServiceRemote.getGoodsDetail(44759,117,"01020115",36146)
+(模板消息  枚举)
+invoke com.xdd.fans.remote.service.message.ITemplateMessageServiceRemote.sendPublicMessage(44759,1212664251910901760,{"name":"REGIMENT_LEADER_OPEN_SUCCESS","class":"com.xdd.fans.remote.enums.MessageTemplateEnum"},["五粮液精品38°酒", "237.1元","明之晓", "2", "2020-01-16 12:04:06"],"https://cpm.cs.kemai.com.cn/webminishop/wxAuth?gonum=orderDetail&orderId=971579143846904143334517","wxcb2208ba8d8fb07c")
+
+
+
+invoke com.xdd.fans.remote.service.message.ITemplateMessageServiceRemote.sendPublicMessage(44759,1212664251910901760,{"name":"REGIMENT_LEADER_OPEN_SUCCESS","class":"com.xdd.fans.remote.enums.MessageTemplateEnum"},["报告老板", "目前还没有好","等待下一步指令", "2", "2020-01-16 12:04:06"],"https://cpm.cs.kemai.com.cn/webminishop/wxAuth?gonum=orderDetail&orderId=971579143846904143334517","wxcb2208ba8d8fb07c")
+
+1212664222789849088
+
+
+invoke com.xdd.fans.remote.service.message.ITemplateMessageServiceRemote.sendPublicMessage(44759,1212664251910901760,{"name":"ASSEMBLE_FAIL","class":"com.xdd.fans.remote.enums.MessageTemplateEnum"},["237.1元","五粮液精品38°酒", "971579143846904143334517"],"https://cpm.cs.kemai.com.cn/webminishop/wxAuth?gonum=orderDetail&orderId=971579143846904143334517","wxcb2208ba8d8fb07c")
+
+storeProductId=1173665&storeId=36146&shopId=31392
+
+
+
+商品20880
+invoke com.xdd.epr.product.remote.service.rpc.IVmallStoreProductServiceRemoteV2.querySkuByUpcList(44759,36146,["6901028055024","6926095512119"])
+
+
+
+
+invoke com.xdd.epr.product.remote.service.rpc.IVmallStoreProductServiceRemoteV2.queryVmallStoreProductVo(44759,36146,1173665)
